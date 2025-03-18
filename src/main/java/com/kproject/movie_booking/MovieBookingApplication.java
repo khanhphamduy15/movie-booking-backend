81cb15589a2d8f2a4111d4cf32063c9e304eab81
Pham Duy Khanh <142990084+khanhphamduy15@users.noreply.github.com>
2025-03-18 13:54:56 +0700

    Initial commit

---
+++ src/main/java/com/kproject/movie_booking/MovieBookingApplication.java
@@ -0,0 +1,13 @@
+package com.kproject.movie_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingApplication.class, args);
+	}
+
+}
